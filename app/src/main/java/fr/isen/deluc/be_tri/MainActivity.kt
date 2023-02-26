6436acdd51507fb061c27464df98d1214aea9c9a
AlexandreDeluc <alexandre.deluc07@gmail.com>
2023-02-26 14:41:54 +0100

    first commit

---
+++ app/src/main/java/fr/isen/deluc/be_tri/MainActivity.kt
@@ -0,0 +1,67 @@
+package fr.isen.deluc.be_tri
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.core.content.FileProvider
+import fr.isen.deluc.be_tri.databinding.ActivityMainBinding
+import java.io.File
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding;
+    lateinit var photoPath: String;
+    val REQUEST_TAKE_PHOTO = 1;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+
+        scanFunction();
+    }
+    private fun scanFunction(){
+        binding.scanButton.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if(intent.resolveActivity(packageManager) != null) {
+                var photoFile: File? = null
+                try {
+                    photoFile = createImageFile()
+                } catch (e:IOException) { }
+                if(photoFile != null) {
+                    val photoUri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.android.fileprovider",
+                        photoFile
+                    )
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+                    startActivityForResult(intent, 1)
+                    true
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_TAKE_PHOTO && resultCode == Activity.RESULT_OK) {
+            binding.image.setImageURI(Uri.parse(photoPath))
+        }
+    }
+
+    private fun createImageFile(): File? {
+        val fileName = "MyPicture"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+            fileName,
+            ".jpg",
+            storageDir,
+        )
+        photoPath = image.absolutePath
+
+        return image
+    }
+}
